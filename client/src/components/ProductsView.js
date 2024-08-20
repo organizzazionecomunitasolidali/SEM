612f7b358198903e2357caf12eaf08f45a5c7e87
php-wizard <lianobat@hotmail.it>
2024-08-20 21:38:03 +0200

    Test autocompletamento

--- client/src/components/ProductsView.js
+++ client/src/components/ProductsView.js
@@ -192,6 +192,9 @@ const ProductsView = () => {
       } else {
         setAnchorEl(null);
       }
+      if (searchFieldRef.current) {
+        searchFieldRef.current.focus();
+      }
     } catch (error) {
       console.error(
         'There has been a problem with your fetch operation:',
@@ -266,9 +269,9 @@ const ProductsView = () => {
             <TextField
               id='searchTerms'
               label={t('Search')}
-              //onChange={handleSearchChange}
-              onChange={(event) => (search = event.target.value)}
-              onKeyUp={(event) => { if (event.key === 'Enter' || event.keyCode === 13) fetchProductData(); } }
+              onChange={handleSearchChange}
+              //onChange={(event) => (search = event.target.value)}
+              onKeyUp={(event) => { console.log("keycode:" + event.keyCode); if (event.key === 'Enter' || event.keyCode === 13) fetchProductData(); } }
               variant="outlined"
               inputRef={searchFieldRef} // Assign the ref to the TextField
               InputLabelProps={{
