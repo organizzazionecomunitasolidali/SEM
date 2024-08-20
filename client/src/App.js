e5647e85b59245b7bcfaeec24cd372fdbafbcfb8
php-wizard <lianobat@hotmail.it>
2024-08-20 19:59:03 +0200

    Login mostrare solo se su sezione admin o loggati

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React , {useContext} from 'react';
 import Login from './components/Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TaskManager from './components/TaskManager';
