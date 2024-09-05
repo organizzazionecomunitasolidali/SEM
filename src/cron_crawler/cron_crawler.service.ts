e9f5c30864c3963aee454c5c42aff977f806bd7a
php-wizard <lianobat@hotmail.it>
2024-09-05 21:29:46 +0200

    force recrawl of some sites

--- src/cron_crawler/cron_crawler.service.ts
+++ src/cron_crawler/cron_crawler.service.ts
@@ -192,6 +192,11 @@ export class CronCrawlerService {
             '',
           );
 
+          if(website.id != 3){
+            // temp
+            await this.semProductService.deleteOlderThan(timestampMs, website, false);
+          }
+
           await this.crawl(website);
 
           // const websiteUpdated = await this.semWebsiteService.findOne(
