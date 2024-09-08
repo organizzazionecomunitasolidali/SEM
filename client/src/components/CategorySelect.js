d418c5c33e7d39d8d96a60d0fc0f6c232acc1ba4
php-wizard <lianobat@hotmail.it>
2024-09-08 19:06:22 +0200

    filters on left pane on large desktop

--- client/src/components/CategorySelect.js
+++ client/src/components/CategorySelect.js
@@ -67,82 +67,83 @@ const CategorySelect = ({ isOnLeftPane, setCategories, selectedItems, setSelecte
   return (
     <>
       {isOnLeftPane ? (
+        <div>
+          <Menu
+                id="simple-menu"
+                open={true} // Always open
+                keepMounted
+                style={{ display: 'block', position: 'static' }} // Ensure it's displayed as a block element and not floating
+          >       
+            {items.map((item) => {
+              const itemLabel = [t(item.name)]
+                .filter(Boolean)
+                .join(' ');
 
-      <div>
-
-        <Button
-          aria-controls="simple-menu"
-          aria-haspopup="true"
-          onClick={handleClick}
-          sx={{
-            color: 'black',
-            padding: '15px',
-            border: '1px solid #aaa',
-            '&:hover': {
-              borderColor: '#35a455', // Apply border color on hover
-              backgroundColor: 'white',
-            },
-            width: isMobile ? '100%' : '200px',
-          }}
-        >
-          {t('Categories')} ▼
-        </Button> &&
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          keepMounted
-          open={Boolean(anchorEl)}
-          onClose={() => setAnchorEl(null)}
-        >
-          {items.map((item) => {
-            const itemLabel = [t(item.name)]
-              .filter(Boolean)
-              .join(' ');
-
-            return (
-              <MenuItem key={item.id} onClick={() => handleToggle(item.id)}>
-                <Checkbox
-                  style={{
-                    color: '#35a455',
-                  }}
-                  checked={selectedItems.includes(item.id)}
-                />
-                {itemLabel || 'Unnamed Item'}
-              </MenuItem>
-            );
-          })}
-        </Menu>
-
-      </div>
+              return (
+                <MenuItem key={item.id} onClick={() => handleToggle(item.id)}>
+                  <Checkbox
+                    style={{
+                      color: '#35a455',
+                    }}
+                    checked={selectedItems.includes(item.id)}
+                  />
+                  {itemLabel || 'Unnamed Item'}
+                </MenuItem>
+              );
+            })}
+          </Menu>
+        </div>      
     
-    ) : (
-      <div>
-      <Menu
-            id="simple-menu"
-            open={true} // Always open
-            keepMounted
-            style={{ display: 'block', position: 'static' }} // Ensure it's displayed as a block element and not floating
-      >       
-        {items.map((item) => {
-          const itemLabel = [t(item.name)]
-            .filter(Boolean)
-            .join(' ');
+      ) : (
 
-          return (
-            <MenuItem key={item.id} onClick={() => handleToggle(item.id)}>
-              <Checkbox
-                style={{
-                  color: '#35a455',
-                }}
-                checked={selectedItems.includes(item.id)}
-              />
-              {itemLabel || 'Unnamed Item'}
-            </MenuItem>
-          );
-        })}
-      </Menu>
-    </div>
-    )}
+        <div>
+
+          <Button
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+            sx={{
+              color: 'black',
+              padding: '15px',
+              border: '1px solid #aaa',
+              '&:hover': {
+                borderColor: '#35a455', // Apply border color on hover
+                backgroundColor: 'white',
+              },
+              width: isMobile ? '100%' : '200px',
+            }}
+          >
+            {t('Categories')} ▼
+          </Button> &&
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={() => setAnchorEl(null)}
+          >
+            {items.map((item) => {
+              const itemLabel = [t(item.name)]
+                .filter(Boolean)
+                .join(' ');
+
+              return (
+                <MenuItem key={item.id} onClick={() => handleToggle(item.id)}>
+                  <Checkbox
+                    style={{
+                      color: '#35a455',
+                    }}
+                    checked={selectedItems.includes(item.id)}
+                  />
+                  {itemLabel || 'Unnamed Item'}
+                </MenuItem>
+              );
+            })}
+          </Menu>
+
+        </div>
+        
+      )}
     
     </>
     
--- client/src/components/UsedOrNewSelect.js
+++ client/src/components/UsedOrNewSelect.js
@@ -21,7 +21,57 @@ const UsedOrNewSelect = ({ isOnLeftPane, selectedUsedOrNew, setSelectedUsedOrNew
   return (
     <>
       {isOnLeftPane ? (
+        
+        <div>
 
+          <Menu
+                id="simple-menu"
+                open={true} // Always open
+                keepMounted
+                style={{ display: 'block', position: 'static' }} // Ensure it's displayed as a block element and not floating
+          >         
+            <MenuItem key='newFirst' onClick={() => handleToggle('newFirst')}>
+                <Radio
+                style={{
+                    color: '#35a455',
+                }}
+                checked={selectedUsedOrNew == 'newFirst'}
+                />
+                {t('newFirst')}
+            </MenuItem>    
+            <MenuItem key='usedFirst' onClick={() => handleToggle('usedFirst')}>
+                <Radio
+                style={{
+                    color: '#35a455',
+                }}
+                checked={selectedUsedOrNew == 'usedFirst'}
+                />
+                {t('usedFirst')}
+            </MenuItem>   
+            <MenuItem key='newOnly' onClick={() => handleToggle('newOnly')}>
+                <Radio
+                style={{
+                    color: '#35a455',
+                }}
+                checked={selectedUsedOrNew == 'newOnly'}
+                />
+                {t('newOnly')}
+            </MenuItem>   
+            <MenuItem key='usedOnly' onClick={() => handleToggle('usedOnly')}>
+                <Radio
+                style={{
+                    color: '#35a455',
+                }}
+                checked={selectedUsedOrNew == 'usedOnly'}
+                />
+                {t('usedOnly')}
+            </MenuItem> 
+          </Menu>
+
+        </div>
+        
+      ) : (
+        
         <div>
           <Button
             aria-controls="simple-menu"
@@ -85,57 +135,8 @@ const UsedOrNewSelect = ({ isOnLeftPane, selectedUsedOrNew, setSelectedUsedOrNew
             </MenuItem>           
           </Menu>
 
-        </div>      
-      ) : (
-        
-        <div>
+        </div>    
 
-          <Menu
-                id="simple-menu"
-                open={true} // Always open
-                keepMounted
-                style={{ display: 'block', position: 'static' }} // Ensure it's displayed as a block element and not floating
-          >         
-            <MenuItem key='newFirst' onClick={() => handleToggle('newFirst')}>
-                <Radio
-                style={{
-                    color: '#35a455',
-                }}
-                checked={selectedUsedOrNew == 'newFirst'}
-                />
-                {t('newFirst')}
-            </MenuItem>    
-            <MenuItem key='usedFirst' onClick={() => handleToggle('usedFirst')}>
-                <Radio
-                style={{
-                    color: '#35a455',
-                }}
-                checked={selectedUsedOrNew == 'usedFirst'}
-                />
-                {t('usedFirst')}
-            </MenuItem>   
-            <MenuItem key='newOnly' onClick={() => handleToggle('newOnly')}>
-                <Radio
-                style={{
-                    color: '#35a455',
-                }}
-                checked={selectedUsedOrNew == 'newOnly'}
-                />
-                {t('newOnly')}
-            </MenuItem>   
-            <MenuItem key='usedOnly' onClick={() => handleToggle('usedOnly')}>
-                <Radio
-                style={{
-                    color: '#35a455',
-                }}
-                checked={selectedUsedOrNew == 'usedOnly'}
-                />
-                {t('usedOnly')}
-            </MenuItem> 
-          </Menu>
-
-        </div>
-        
     )}
     
     </>
