86a7c0f9bff24540425653778ea69c899828f3bd
php-wizard <lianobat@hotmail.it>
2024-09-09 18:59:44 +0200

    Left pane

--- client/src/components/UsedOrNewSelect.js
+++ client/src/components/UsedOrNewSelect.js
@@ -22,7 +22,7 @@ const UsedOrNewSelect = ({ isOnLeftPane, selectedUsedOrNew, setSelectedUsedOrNew
     <>
       {isOnLeftPane ? (
         
-        <div style={{marginBottom: "30px", boxShadow: "2px 2px 20px #2222"}}>
+        <div style={{marginBottom: "30px", boxShadow: "2px 2px 20px #2222", padding: "10px"}}>
           
             {/* Title/Label in bold */}
             <Typography variant="h6" style={{ fontWeight: 'bold', padding: '10px 16px' }}>
