409050250215a9e4b224c03d2fcb5550a42a3819
php-wizard <lianobat@hotmail.it>
2024-12-16 02:47:15 +0100

    Webp

--- src/cron_crawler/cron_crawler.service.ts
+++ src/cron_crawler/cron_crawler.service.ts
@@ -990,7 +990,7 @@ export class CronCrawlerService {
               console.log(
                 'updateProductThumbnail ' + product.url + ' ' + productStructure.thumbnailUrl,
               );
-              if(product.thumbnail_url != productStructure.thumbnailUrl){
+              if(true || product.thumbnail_url != productStructure.thumbnailUrl){
                 await this.semProductService.updateProductThumbnail(product, productStructure.thumbnailUrl, true);
               }
             } else {
