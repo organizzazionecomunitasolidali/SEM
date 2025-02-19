4730260307745ca16ab45d44863fe7f2a6684b13
lbattt <battistellaliano@gmail.com>
2025-02-19 05:35:08 +0100

    Random ordering

--- src/entities/sem_product.service.ts
+++ src/entities/sem_product.service.ts
@@ -268,6 +268,16 @@ export class SemProductService {
     if (!existingThumbnail || !fs.existsSync(imagePath)) {
       existingThumbnail = null;
     }
+
+    // temp debug
+    if(product.website.id == 1){
+      console.log("updateProductThumbnail existingThumbnail:" + existingThumbnail);
+      console.log("updateProductThumbnail url:" + product.url);
+      console.log("updateProductThumbnail product_thumbnail_url:" + product_thumbnail_url);
+      console.log("updateProductThumbnail force_update_if_existing:" + force_update_if_existing);
+    }
+    // temp debug end
+
     if(!existingThumbnail || force_update_if_existing){
 
       const no_image_url = "file://" + path.join(getClientDir(), 'image_not_found.png');
