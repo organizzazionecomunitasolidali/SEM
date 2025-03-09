826a7e0bb51607f5779d284fc2f7bbf3c31a758d
lbattt <battistellaliano@gmail.com>
2025-03-09 16:41:31 +0100

    Alternative currency or complementary currency flag

--- client/src/components/TaskView.js
+++ client/src/components/TaskView.js
@@ -240,7 +240,7 @@ const TaskView = ({
       title: 'Is second currency complementary (true) or alternative (false)?',
       field: 'is_second_currency_complementary',
       editor: 'checkbox',
-      width: 500,
+      width: 450,
       headerFilter: 'checkbox',
     }    
   ];
